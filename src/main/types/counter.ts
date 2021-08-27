3beb3586a3135898b57d8f26991423bc5de27d91
Olivier Penhoat <openhoat@gmail.com>
2021-08-27 15:09:03 +0200

    Add counter test

---
+++ src/main/types/counter.ts
@@ -0,0 +1,1 @@
+export type CounterFactory = () => () => number
---
+++ src/test/counter.test.ts
@@ -0,0 +1,35 @@
+import { basename } from 'path'
+
+import { CounterFactory } from '../main/types/counter'
+
+/**
+ * @group sync
+ * @group supereasy
+ */
+
+const modName = basename(__filename, '.test.ts')
+
+describe('counter', () => {
+  describe('counter factory', () => {
+    let counterFactory: CounterFactory
+    beforeAll(() => {
+      counterFactory = require(`../main/${modName}`).default
+    })
+    describe('increase counter', () => {
+      let increaseCounter: () => number
+      beforeAll(() => {
+        increaseCounter = counterFactory()
+      })
+      test('should increase counter value 20 times', () => {
+        Array.from(Array(20))
+          .map((__, index) => index + 1)
+          .forEach((expectedResult) => {
+            // When
+            const result = increaseCounter()
+            // Then
+            expect(result).toBe(expectedResult)
+          })
+      })
+    })
+  })
+})
